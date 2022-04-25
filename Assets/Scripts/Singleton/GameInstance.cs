2f9030d02bd00ace3c312fb8930f7f3f8881ae59
Adolys <beomsun96@gmail.com>
2022-04-25 22:16:43 +0900

    singleton work
    singleton templete add
    GameInstance add

---
+++ Assets/Scripts/Singleton/GameInstance.cs
@@ -0,0 +1,25 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstance : Singleton<GameInstance>
+{
+    protected int characterIndex;
+
+    public GameInstance()
+    {
+        characterIndex = 0;
+
+        Debug.Log("Call GameInstace constructor!");
+    }
+
+    public int GetCharacterIndex()
+    {
+        return characterIndex;
+    }
+
+    public void SetCharacterIndex(int index)
+    {
+        characterIndex = index;
+    }
+}
